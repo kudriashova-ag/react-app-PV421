e5581451889d880f1d76455aa8755f33d605156d
Anastasiia Kudriashova <kudriashova.ag@gmail.com>
2025-06-25 19:53:04 +0300

    Formik Error Message

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,9 @@
-import { Field, Form, Formik } from "formik";
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import React from "react";
 import * as Yup from "yup";
+import styles from "./Register.module.css";
+
+
 
 const initialValues = {
   name: "",
@@ -13,8 +16,11 @@ const RegisterSchema = Yup.object().shape({
   name: Yup.string().min(2, "Too Short").required(),
   email: Yup.string().email().required(),
   password: Yup.string()
-    .matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/, "Too Weak")
-        .required(),
+    .matches(
+      /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/,
+      "Too Weak"
+    )
+    .required(),
   confirmPassword: Yup.string()
     .oneOf([Yup.ref("password"), null], "Passwords must match")
     .required(),
@@ -27,27 +33,36 @@ const Register = () => {
 
   return (
     <div>
-      <h1>Register</h1>
-          <Formik
-              initialValues={initialValues}
-              onSubmit={submitHandler}
-              validationSchema={RegisterSchema}>
+      <h1 className={styles.title}>Register</h1>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={submitHandler}
+        validationSchema={RegisterSchema}
+      >
         {() => (
           <Form>
             <div>
               <Field name="name" placeholder="Name" />
+              <ErrorMessage name="name" component="div" className="error" />
             </div>
             <div>
               <Field type="email" name="email" placeholder="Email" />
+              <ErrorMessage name="email" component="div" className="error" />
             </div>
             <div>
               <Field type="password" name="password" placeholder="Password" />
+              <ErrorMessage name="password" component="div" className="error" />
             </div>
             <div>
               <Field
                 type="password"
                 name="confirmPassword"
                 placeholder="Confirm Password"
+              />
+              <ErrorMessage
+                name="confirmPassword"
+                component="div"
+                className="error"
               />
             </div>
             <button type="submit">Register</button>
